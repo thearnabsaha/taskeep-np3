538d06bfe1492c3dd9247321fe1d39b74bc34a4e
thearnabsaha <thearnabsaha201@gmail.com>
2022-01-22 16:14:39 +0530

    s6

--- pages/Components/Notes.jsx
+++ pages/Components/Notes.jsx
@@ -1,16 +1,16 @@
 import { MdOutlineDelete } from "react-icons/md";
 const Notes = () => {
-    const notes = [{title:"arnab",content:"saha",key:1},{title:"arnab",content:"saha",key:1},{title:"arnab",content:"saha",key:1},{title:"arnab",content:"saha",key:1},]
+    const notes = [{title:"arnab",content:"saha"},{title:"arnab",content:"saha"},{title:"arnab",content:"saha"},{title:"arnab",content:"saha"},]
     const deleteItem=() => {
-        
+        alert("this function haven't added yet")
     }
     return (
         <>
             <div className="notes">
                 {
-                    notes.map((e)=>{
+                    notes.map((e,ind)=>{
                         return(
-                            <div className="note" key={e.key}>
+                            <div className="note" key={ind}>
                                 <div className="content">
                                     <h2>{e.title}</h2>
                                     <p>{e.content}</p>
