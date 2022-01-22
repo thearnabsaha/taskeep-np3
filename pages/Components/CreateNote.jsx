b40f5b4caef0cf49d5a357840bcb7576345419c8
thearnabsaha <thearnabsaha201@gmail.com>
2022-01-22 11:21:05 +0530

    s1

--- pages/Components/CreateNote.jsx
+++ pages/Components/CreateNote.jsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 import { MdAdd } from "react-icons/md";
 const CreateNote = () => {
     const [data, setData] = useState({
-        
+        title:"",
+        content:"",
     });
+
     return (
         <>
             <div className="createNote">
             <form action="">
-                <input type="text" name="title" value="" onChange={changeHandler}/>
-                <textarea name="content" value="" onChange={changeHandler}/>
+                <input type="text" name="title" value={data.title} onChange={changeHandler} autoComplete="off" placeholder="Title" />
+                <textarea name="content" value={data.content} onChange={changeHandler} placeholder="Type you note here..." />
                 <button><MdAdd/></button>
             </form>
             </div>
