705157dc8dfa98a9977fbd3e0b95a4fc674dc22e
thearnabsaha <thearnabsaha201@gmail.com>
2022-01-22 11:00:08 +0530

    s1

---
+++ pages/Components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <>
+            <div className="header">
+                <h1>TAS KEEP</h1>
+            </div>
+        </>
+    );
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head';
+import Header from './Components/Header';
 const index = () => {
   return (
     <>
       <Head>
         <title>TAS Keep</title>
       </Head>
+      <Header/>
     </>
   );
 }
