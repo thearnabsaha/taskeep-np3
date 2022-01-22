1f00906b7425978ec1cef50ff0a1a568725d5d32
thearnabsaha <thearnabsaha201@gmail.com>
2022-01-22 11:35:08 +0530

    s1

--- pages/Components/Notes.jsx
+++ pages/Components/Notes.jsx
@@ -2,7 +2,8 @@ const Notes = () => {
     return (
         <>
             <div className="notes">
-                
+                <h2>title</h2>
+                <p>content</p>
             </div>
         </>
     );
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import CreateNote from './Components/CreateNote';
+import Notes from './Components/Notes';
 const index = () => {
   return (
     <>
@@ -11,6 +12,7 @@ const index = () => {
       <Header/>
       <CreateNote/>
       <Footer/>
+      <Notes/>
     </>
   );
 }
